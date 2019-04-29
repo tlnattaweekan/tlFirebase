37aa77cd1b29c2721088e59784d0d425c2cc3080
nattaweekan_TL <nattaweekan.w@gmail.com>
2019-04-29 15:33:48 +0700

    Color Gradient fonts logo

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,18 +6,43 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showAppName() {
+    return Text('Master Flutter',
+        style: TextStyle(
+          fontFamily: 'AmaticSC',
+          fontSize: 35.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.brown,
+        ));
+  }
 
-  Widget showAppName(){
-    return Text('Master Flutter');
+  Widget showLogo() {
+    return Image.asset('images/logo.png');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.blueGrey[300], Colors.grey[300]],
+              begin: Alignment(-1, -1))),
       padding: EdgeInsets.only(top: 100.0),
-      alignment: Alignment(-1, 1),
-      child: showAppName(), //call showAppName
+      alignment: Alignment(0, -1),
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: 200.0,
+            height: 200.0,
+            child: showLogo(),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 0.0),
+            child: showAppName(),
+          )
+        ],
+      ), //call showAppName
     ));
   }
 }
